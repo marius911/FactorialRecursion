972116903e99a058a4dbca2a8174c67e33d66ce4
Marius Iulian <marius.ancutescu@outlook.com>
2020-03-10 20:23:15 +0200

    Add project files.

---
+++ FactorialRecursion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FactorialRecursion
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Factorial(Convert.ToInt32(Console.ReadLine())));
+        }
+
+        static int Factorial(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
